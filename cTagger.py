e4a705a551e6dfe85e51b7111b2d98faa7ad5f2d
Jeppe Terndrup <jeppe.terndrup@gmail.com>
2012-12-17 04:59:19 +0100

    Added Errorhandling on "File Not found"

--- cTagger.py
+++ cTagger.py
@@ -6,27 +6,42 @@ import re
 import sys
 
 """importText
-Opens the file specified by the user using the 'rU' flag. Returns a FileNotFounderror
+Opens the file specified by the user using the 'rU' flag. Returns an error
 if the file is not found.
 """
 def importText(location):
-    f = open(location,'rU');
-    text = f.read();
-    # print text;     - debug
-    f.close();
+    try:
+        f = open(location,'rU');       
+    except IOError:
+        print "Error: The file was not found";
+        inputPrompt();
+    else:
+        text = f.read();
+        # print text;      - debug
+        f.close();
 
-    # Use a regular expression matching a full-stop not preceded by an integer (numeric)
-    # to split the text into full sentences.
-    sentences = re.split('\s+([.][/]RESID_SIGN)+',text);
+        # Use a regular expression matching a full-stop not preceded by an integer (numeric)
+        # to split the text into full sentences.
+        sentences = re.split('\s+([.][/]RESID_SIGN)+',text);
 
-    # Since we've split by the above expression, we add it to each sentence and remove stand-alones
-    tempSen  = filter(lambda x: x != "./RESID_SIGN", sentences);
-    finalSen = [];
-    for s in tempSen:
-        finalSen.append(s + " ./RESID_SIGN");
+        # Since we've split by the above expression, we add it to each sentence 
+        # and remove stand-alones
+        tempSen  = filter(lambda x: x != "./RESID_SIGN", sentences);
+        finalSen = [];
+        for s in tempSen:
+            finalSen.append(s + " ./RESID_SIGN");
 
-    # print finalSen;   - debug
-    
+        # print finalSen;   - debug
+        
+ 
+"""SortTags(List)
+Takes a list of sentences and cuts each word in the sentence from its' tag, then places
+word and tag into a key/value dictionary. Returns a list of dictionaries, each representing
+an original sentence 
+"""
+   
+# def SortTags(sentenceList):
+            
 
 """inputPrompt - Prompts the user for terminal input containing the absolute path 
 of a readable file, using the raw_input function. 
