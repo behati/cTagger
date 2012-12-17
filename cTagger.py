cc28145ecac4d9d02d53511f8000a94331cc7fb1
Jeppe Terndrup <jeppe.terndrup@gmail.com>
2012-12-17 09:59:31 +0100

    restore

--- cTagger.py
+++ cTagger.py
@@ -13,39 +13,39 @@ if the file is not found.
 """
 def importText(location):
     try:
-        f = open(location,'rU');       
+        f = open(location,'rU');
     except IOError:
         print "Error: The file was not found";
         inputPrompt();
     else:
         text = f.read();
-        # print text;       - debug
+        # print text; - debug
         f.close();
 
         # Use a regular expression matching a full-stop not preceded by an integer (numeric)
         # to split the text into full sentences.
         sentences = re.split('\s+([.][/]RESID_SIGN)+',text);
 
-        # Since we've split by the above expression, we add it to each sentence 
+        # Since we've split by the above expression, we add it to each sentence
         # and remove stand-alones
-        tempSen  = filter(lambda x: x != "./RESID_SIGN", sentences);
-        fixSen   = tempSen[:-1];
+        tempSen = filter(lambda x: x != "./RESID_SIGN", sentences);
+        fixSen = tempSen[:-1];
         finalSen = [];
         for s in fixSen:
             finalSen.append(s + " ./RESID_SIGN");
 
-        # print finalSen;   - debug
+        # print finalSen; - debug
         SortTags(finalSen);
  
 """SortTags(List)
 Takes a list of sentences and cuts each word in the sentence from its' tag, then places
 word and tag into a key/value dictionary. Returns a list of dictionaries, each representing
-an original sentence. Prints a progressbar to the terminal to show progress. 
+an original sentence. Prints a progressbar to the terminal to show progress.
 """
    
 def SortTags(sentenceList):
     TaggedSen = [];
-    TagDict   = {};
+    TagDict = {};
     print "Processing... \n";
     pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=100).start(); # progressbar-module
     i = 0;
@@ -58,18 +58,18 @@ def SortTags(sentenceList):
                 TagDict[tagged[0]] = tagged[1];
 
         TaggedSen.append(TagDict);
-        # print TagDict;    - debug 
+        # print TagDict; - debug
         TagDict = {};
         pbar.update( i * 100 / len(sentenceList)); # progressbar-module
         i = i+1;
 
     pbar.finish();
 
-    # print TaggedSen;     - debug          
+    # print TaggedSen; - debug
  
 
-"""inputPrompt - Prompts the user for terminal input containing the absolute path 
-of a readable file, using the raw_input function. 
+"""inputPrompt - Prompts the user for terminal input containing the absolute path
+of a readable file, using the raw_input function.
 """
 def inputPrompt():
     filepath = raw_input("File: ");
@@ -91,4 +91,3 @@ def main():
     inputPrompt();
 if __name__ =='__main__':
     main();
-
